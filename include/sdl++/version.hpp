51342d0fb0b24ae6dd07be2fd3ccf12f54c61576
Tristan Brindle <t.c.brindle@gmail.com>
2016-01-15 23:49:19 +1300

    Use Boost-style documentation comments
    Boost projects seem to use Qt-style comment tags combined with
    JavaDoc-style Doxygen directives, i.e.
    
      //! @file header.hpp
    
    and
    
     /*!
      @brief Some documentation
    
      ...
      */
    
    This seems to work pretty well and allow a mixture of one-line
    and multi-line comment blocks without either looking out of place,
    so let's copy it.

--- include/sdl++/version.hpp
+++ include/sdl++/version.hpp
@@ -1,7 +1,9 @@
-/*
-  Simple DirectMedia Layer C++ Bindings
-  Copyright (C) 2016 Tristan Brindle <t.c.brindle@gmail.com>
-  Copyright (C) 1997-2016 Sam Lantinga <slouken@libsdl.org>
+/*!
+  @file version.hpp
+  Functions and classes dealing with SDL versions
+
+  @copyright (C) 2016 Tristan Brindle <t.c.brindle@gmail.com>
+  @copyright (C) 1997-2016 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -27,98 +29,92 @@
 
 #include <iostream>
 
-/**
- *  \file version.hpp
- *
- *  \brief This header contains the functions and classes in the Version module.
- */
-
 namespace sdl {
 
-/**
- * \defgroup Version Querying SDL version
- *
- *  These functions are used to collect or display information about the version
- *  of SDL that is currently being used by the program or that it was compiled
- *  against. The version consists of three segments (X.Y.Z)
- *    * X = Major Version, which increments with massive changes, additions, and
- *    enhancements
- *    * Y = Minor Version, which increments with backwards-compatible changes to
- *      the major revision
- *    * Z = Patch level, which increments with fixes to the minor revision
- *
- *  For example, the current version is 2.0.4
- *
- *  The compile-time SDL version is available with the
- *  sdl::get_compiled_version() function. This is a constexpr function, and
- *  so could be used for conditional compilation with std::enable_if().
- *
- *  Alternatively, the compile-time SDL version is available via the
- *  SDL_COMPILED_VERSION macro, and the individual components via
- *  SDL_MAJOR_VERSION, SDL_MINOR_VERSION and SDL_PATCH_VERSION -- see the
- *  [C documentation](https://wiki.libsdl.org/CategoryVersion).
- *
- *  @{
+/*!
+ @defgroup Version Querying SDL version
+ @{
+
+ These functions are used to collect or display information about the version
+ of SDL that is currently being used by the program or that it was compiled
+ against. The version consists of three segments (X.Y.Z)
+
+ - X = Major Version, which increments with massive changes, additions, and
+     enhancements
+ - Y = Minor Version, which increments with backwards-compatible changes to
+     the major revision
+ - Z = Patch level, which increments with fixes to the minor revision
+
+ For example, the current version is 2.0.4
+
+ The compile-time SDL version is available with the
+ `sdl::get_compiled_version()` function. This is a `constexpr` function, and
+ so could be used for conditional compilation with `std::enable_if()`.
+
+ Alternatively, the compile-time SDL version is available via the
+ `SDL_COMPILED_VERSION` macro, and the individual components via
+ `SDL_MAJOR_VERSION`, `SDL_MINOR_VERSION` and `SDL_PATCH_VERSION` -- see the
+ [C documentation](https://wiki.libsdl.org/CategoryVersion).
  */
 
-/**
- *  \brief Represents an SDL version number
- *
- *  Represents the library's version as three levels: major revision
- *  (increments with massive changes, additions, and enhancements),
- *  minor revision (increments with backwards-compatible changes to the
- *  major revision), and patchlevel (increments with fixes to the minor
- *  revision).
- *
- *  sdl::version can be compared using the usual comparison operators, for
- *  example
- *
- *  ```cpp
- *  sdl::version v1{2, 0, 3};
- *  sdl::version v2{2, 0, 4};
- *
- *  v1 < v2; // true
- *  ```
- *
- *  and can be passed to an std::ostream
- *
- *  ```
- *  sdl::version v = sdl::get_compiled_version();
- *
- *  std::cout << "Compiled with SDL " << v << "\n";
- *  ```
- *
- *  \sa sdl::get_compiled_version()
- *  \sa sdl::get_linked_version()
+/*!
+ @brief Represents an SDL version number
+
+ Represents the library's version as three levels: major revision
+ (increments with massive changes, additions, and enhancements),
+ minor revision (increments with backwards-compatible changes to the
+ major revision), and patchlevel (increments with fixes to the minor
+ revision).
+
+ `sdl::version`s can be compared using the usual comparison operators, for
+ example
+
+ ```
+ sdl::version v1{2, 0, 3};
+ sdl::version v2{2, 0, 4};
+
+ v1 < v2; // true
+ ```
+
+ and can be passed to a `std::ostream`
+
+ ```
+ sdl::version v = sdl::get_compiled_version();
+
+ std::cout << "Compiled with SDL " << v << "\n";
+ ```
+
+ @sa `sdl::get_compiled_version()`
+ @sa `sdl::get_linked_version()`
  */
 struct version : SDL_version {
-     /// Default-constructs an sdl::version to {0, 0, 0}
+    //! Default-constructs an sdl::version to {0, 0, 0}
     constexpr version() : SDL_version{0, 0, 0} {}
 
-    /**
-     * Constructs an sdl::version from the given major, minor and patch levels
-     * \param major The major version number
-     * \param minor The minor version number
-     * \param patch The patch level
-     * \returns A newly constructed sdl::version
+    /*!
+     Constructs an sdl::version from the given major, minor and patch levels
+     @param major The major version number
+     @param minor The minor version number
+     @param patch The patch level
+     @returns A newly constructed sdl::version
      */
     constexpr version(uint8_t major, uint8_t minor, uint8_t patch)
         : SDL_version{major, minor, patch} {}
 };
 
-/// \relates version
+//! @relates version
 inline constexpr bool operator==(version lhs, version rhs) {
     return lhs.major == rhs.major && lhs.minor == rhs.minor &&
            lhs.patch == rhs.patch;
 }
 
-/// \relates version
+//! @relates version
 // TODO(C++14): use std::tie when available
 inline constexpr bool operator!=(version lhs, version rhs) {
     return !(lhs == rhs);
 }
 
-/// \relates version
+//! @relates version
 // TODO(C++14): use std::tie when available
 inline constexpr bool operator<(version lhs, version rhs) {
     return lhs.major == rhs.major
@@ -127,45 +123,43 @@ inline constexpr bool operator<(version lhs, version rhs) {
                : lhs.major < rhs.major;
 }
 
-/// \relates version
-inline constexpr bool operator>(version lhs, version rhs) {
-    return rhs < lhs;
-}
+//! @relates version
+inline constexpr bool operator>(version lhs, version rhs) { return rhs < lhs; }
 
-/// \relates version
+//! @relates version
 inline constexpr bool operator<=(version lhs, version rhs) {
     return !(lhs > rhs);
 }
 
-/// \relates version
+//! @relates version
 inline constexpr bool operator>=(version lhs, version rhs) {
     return !(lhs < rhs);
 }
 
-/// \relates version
+//! @relates version
 inline std::ostream& operator<<(std::ostream& os, version v) {
     return os << int{v.major} << '.' << int{v.minor} << '.' << int{v.patch};
 }
 
-/**
- *  \brief Get the version of SDL that is linked against your program.
- *
- *  If you are linking to SDL dynamically, then it is possible that the
- *  current version will be different than the version you compiled against.
- *  This function returns the current version, while get_compiled_version() is a
- *  function that tells you what version you compiled with.
- *
- *  ```
- *  sdl::version compiled = sdl::get_compiled_version();
- *  sdl::version linked = sdl::get_linked_version();
- *
- *  std::cout << "We compiled against SDL version " << compiled << "\n";
- *  std::cout << "But we linked against SDL version << linked << "\n";
- *  ```
- *
- *  This function may be called safely at any time, even before sdl::init().
- *
- *  \sa sdl::get_compiled_version()
+/*!
+ Get the version of SDL that is linked against your program.
+
+ If you are linking to SDL dynamically, then it is possible that the
+ current version will be different than the version you compiled against.
+ This function returns the current version, while `get_compiled_version()` is a
+ function that tells you what version you compiled with.
+
+ ```
+ sdl::version compiled = sdl::get_compiled_version();
+ sdl::version linked = sdl::get_linked_version();
+
+ std::cout << "We compiled against SDL version " << compiled << "\n";
+ std::cout << "But we linked against SDL version << linked << "\n";
+ ```
+
+ This function may be called safely at any time, even before sdl::init().
+
+ @sa `sdl::get_compiled_version()`
  */
 inline version get_linked_version() {
     version v{0, 0, 0};
@@ -173,41 +167,41 @@ inline version get_linked_version() {
     return v;
 }
 
-/**
- *  \brief Determine SDL version program was compiled against.
- *
- *  This function returns a sdl::version structure with the version of the
- *  library you compiled against. This is determined by what header the
- *  compiler uses. Note that if you dynamically linked the library, you might
- *  have a slightly newer or older version at runtime. That version can be
- *  determined with sdl::get_linked_version()
- *
- *  \sa sdl::version
- *  \sa sdl::get_linked_version()
+/*!
+ Determine SDL version program was compiled against.
+
+ This function returns a sdl::version structure with the version of the
+ library you compiled against. This is determined by what header the
+ compiler uses. Note that if you dynamically linked the library, you might
+ have a slightly newer or older version at runtime. That version can be
+ determined with sdl::get_linked_version()
+
+ @sa `sdl::version`
+ @sa `sdl::get_linked_version()`
  */
 inline constexpr version get_compiled_version() {
     return version{SDL_MAJOR_VERSION, SDL_MINOR_VERSION, SDL_PATCHLEVEL};
 }
 
-/**
- *  \brief Get the code revision of SDL that is linked against your program.
- *
- *  \returns An arbitrary string (a hash value) uniquely identifying the
- *  exact revision of the SDL library in use, and is only useful in comparing
- *  against other revisions. It is NOT an incrementing number.
+/*!
+ @brief Get the code revision of SDL that is linked against your program.
+
+ @returns An arbitrary string (a hash value) uniquely identifying the
+  exact revision of the SDL library in use, and is only useful in comparing
+  against other revisions. It is NOT an incrementing number.
  */
 inline const char* get_revision() { return ::SDL_GetRevision(); }
 
-/**
- *  \brief Get the revision number of SDL that is linked against your program.
- *
- *  \returns A number uniquely identifying the exact revision of the SDL
- *  library in use. It is an incrementing number based on commits to
- *  hg.libsdl.org.
+/*!
+ Get the revision number of SDL that is linked against your program.
+
+ @returns A number uniquely identifying the exact revision of the SDL
+  library in use. It is an incrementing number based on commits to
+  hg.libsdl.org.
  */
 inline int get_revision_number() { return ::SDL_GetRevisionNumber(); }
 
-/// @}
+//! @}
 
 } // end namespace sdl
 
